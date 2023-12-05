72563ed75dafb2cf65325c1c660b2729d8697876
hikaelis <sileakih@gmail.com>
2023-12-05 22:17:32 +0900

    fix generatePrompt.js

--- src/generatePrompt.js
+++ src/generatePrompt.js
@@ -6,59 +6,56 @@ const getRandom = (min, max) => {
 }
 
 const generatePrompt = () => {
+  const base_sentence = `進次郎構文を1つのみ生成.説明無し発言内容のみ「」無し`
   // 主張系
   const claim = `
-    lang: ja
-    issue: 小泉進次郎構文の文を1つ作って。
-    context:
-      type: 1文だけ生成|説明不要|文章のみ|claim
-      persona: 小泉進次郎
-    examples:
-    Always put "body temperature" and "weight" on words.
-    The raw material of plastic is petroleum. Surprisingly, people don't know this.
-    Everything should be fun, cool, and sexy.
-    When Water and oil were mixed, it becomes dressing. This election is all about this remark.
-    Ambiguously I came up with a number, 46.
-    `
-  
+    例)
+    言葉には必ず「体温」と「体重」をのせる
+    プラスチックの原料って石油なんですよね。意外にこれ知られてない
+    楽しく、かっこよく、セクシーであるべきだ
+    水と油、混ぜればドレッシングになると言われた。今回の選挙はこの一言に尽きる
+    おぼろげながら浮かんできたんです、46という数字が
+    調査では増えているから増えたかどうか調査する
+    育児休業という「休む」という言葉が入っていますが、休みではない
+    打倒！ドンキホーテ。打倒！パプリカ
+  `
   // トートロジー系
   const tautology = `
-    lang: ja
-    issue: 小泉進次郎構文の文を1つ作って
-    context:
-      type: 1文だけ生成|説明不要|文章のみ|tautology
-      persona: 小泉進次郎
-    examples:
-    To the point that I don't seem to be remorseful, I remose it. it's my own problem.
-    Thanks to working from home, I can carry out my official work remotely.
-    I was wondering how old I am after 30 years in my mind, just right after that natural disaster.
-    I don't think Japan should stay the way it is now. That's why I think Japan shouldn't stay the way it is now.
+    例)
+    反省している色が見えないという指摘には私自身の問題だと反省している
+    リモートワークができてるおかげで 公務もリモートワークでできてますから
+    私の中で30年後を考えたときに30年後の自分は何歳かなと発災直後から考えていた
+    今のままではいけないと思います だからこそ日本は今のままではいけないと思っている
+    毎日でも食べたいということは毎日でも食べているということではないです
+    野球部員だった私は水筒を使っていたけど、環境配慮の観点で水筒を使っていなかった
     `
   // 大喜利系
   const ogiri = `
-    lang: ja
-    issue: 小泉進次郎構文の文を1つ作って
-    context:
-      type: 1文だけ生成|説明不要|文章のみ|ogiri
-      persona: 小泉進次郎
-    examples:
-    I heard you were born on your birthday.
-    Bakery stores have lots of bread. 
-    Devil is a devil ‼️ Happiness is happiness ‼️
-    Breath of Breath.
-    [House of Representatives Election] My hometown is my hometown.
-    If you wanna watch the night view, I definitely recommend at night. 
-    I'm sure there will be medalists from this Olympic. 
-    What percent of juice is these apples.
-    Underage drinking is something children do.
+  例)
+    君は誕生日に生まれたんだって？
+    しかしパン屋って本当にパンが多いね
+    鬼は、鬼‼️ 福は、福‼️
+    息の呼吸
+    【衆院選】地元がホームタウン
+    夜景を見るなら断然夜をオススメしますよ
+    出場者からメダリストが出るでしょうね
+    東京オリンピックの終わりを予感させる、そんな閉会式でしたね
+    このりんごは果汁何パーセントくらいですか？
+    未成年飲酒なんて子供のすることですよ
+    1階と2階が別の階になってるんですね。
+    どの映画を観ても必ず最後にラストシーンが来るんですよね
+    私から見て左側が左でしたっけ？
+    このワンちゃんは犬で言うと何歳なんですか？
+    どうやら高学歴な人ほど学歴が高いみたいなんですよね
     `
 
-  const prompts = [claim, tautology, ogiri]
-  const prompt = prompts[getRandom(0,2)];
-  
+  const examples = [claim, tautology, ogiri]
+  const example = examples[getRandom(0,2)];
+  const prompt = base_sentence + example
   return prompt
 }
 
-// generatePrompt();
+// const prompt = generatePrompt();
+// console.log(prompt)
 
 module.exports = {generatePrompt}
